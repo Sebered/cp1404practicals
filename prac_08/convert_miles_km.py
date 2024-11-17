7020413e83ab8d3aa225db29417f5de123890571
claym <c.millburn04@gmail.com>
2024-11-17 14:15:05 +1000

    mile convert prac updated

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,21 +13,21 @@ class MilesConverterApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self, text):
-        miles = self.convert_to_number(text)
+    def handle_calculate(self, value):
+        miles = self.convert_to_number(value)
         self.update_result(miles)
 
-    def handle_increment(self, text, change):
-        miles = self.convert_to_number(text) + change
+    def handle_increment(self, value, change):
+        miles = self.convert_to_number(value) + change
         self.root.ids.input_miles.text = str(miles)
 
     def update_result(self, miles):
         self.output_km = str(miles * MILES_TO_KM)
 
     @staticmethod
-    def convert_to_number(text):
+    def convert_to_number(value):
         try:
-            return float(text)
+            return float(value)
         except ValueError:
             return 0.0
 
